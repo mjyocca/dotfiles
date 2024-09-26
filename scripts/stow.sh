826a65c3856d00430ef419c09144578e2ab4698b
Michael Yocca <mjyocca@gmail.com>
2024-09-25 17:25:43 -0700

    chore(scripts): update stow verbose flag

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -3,7 +3,7 @@
 stow_dotfiles() {
   local to_stow="$(find stow -maxdepth 1 -type d -mindepth 1 | awk -F "/" '{print $NF}' ORS=' ')"
 	echo "Stowing: $to_stow"
-	stow --dir stow --verbose 1 --target "$HOME" $to_stow --adopt
+	stow --dir stow --verbose 2 --target "$HOME" $to_stow
 }
 
 stow_dotfiles
