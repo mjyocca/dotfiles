425995f709d59429d88385db86d9cbc11911ec5a
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2024-10-15 22:30:21 -0700

    fix(osx): configure osx remap caps to esc

--- osx/config.sh
+++ osx/config.sh
@@ -13,6 +13,31 @@ setup_osx() {
 	launchctl unload "$HOME/Library/LaunchAgents/com.openssh.ssh-agent.plist"
 	launchctl load "$HOME/Library/LaunchAgents/com.openssh.ssh-agent.plist"
 	launchctl enable gui/$(id -u)/com.openssh.ssh-agent
+
+	info "Remapping Caps to Esc"
+	cp "$HOME/dotfiles/osx/com.local.remap_caps_lock.plist" "$HOME/Library/LaunchAgents/"
+	launchctl unload "$HOME/Library/LaunchAgents/com.local.remap_caps_lock.plist"
+	launchctl load "$HOME/Library/LaunchAgents/com.local.remap_caps_lock.plist"
+	launchctl enable gui/$(id -u)/com.local.remap_caps_lock
+}
+
+remap_keyboard_shortcut() {
+  devices_json="$(hidutil list -m 'keyboard' -n)"
+  keyboard_devices=$(echo "$devices_json" | jq -s 'group_by(.ProductID) | map({Product: .[0].Product, ProductID: .[0].ProductID, VendorID: .[0].VendorID }) | del(.[] | select(.ProductID==0 or .ProductID==null))')
+  echo "$keyboard_devices"
+
+  # echo "$keyboard_devices" | jq -c '.[]' | while read -r item; do
+  #   # Extract values using jq
+  #   product_name=$(echo "$item" | jq '.Product')
+  #   product_id=$(echo "$item" | jq '.ProductID')
+  #   vendor_id=$(echo "$item" | jq '.VendorID')
+
+  #   # Print the id and value
+  #   echo "Product: $product_name, ProductID: $product_id, VendorID: $vendor_id"
+  #   defaults -currentHost write -g com.apple.keyboard.modifiermapping."$vendor_id"-"$product_id"-0 -array-add '{"HIDKeyboardModifierMappingDst"=30064771113; "HIDKeyboardModifierMappingSrc"=30064771129; }'
+  # done
+  # info "Writing mapping for built-in"
+  # defaults -currentHost write -g com.apple.keyboard.modifiermapping.0-0-0 -array-add '{"HIDKeyboardModifierMappingDst"=0x700000029; "HIDKeyboardModifierMappingSrc"=0x700000039; }'
 }
 
 # Check if the script is being run directly or sourced
