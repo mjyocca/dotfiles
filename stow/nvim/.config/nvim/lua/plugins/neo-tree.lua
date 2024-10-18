db7deb79d326c6885f0d93891e9fc90fd11a974b
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2024-10-17 22:34:20 -0700

    chore(nvim): set neotree nvim keymap to silent

--- stow/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ stow/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -40,7 +40,11 @@ return {
         auto_expand = true,
       },
     })
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {
+      desc = 'NeoTree reveal', silent = true
+    })
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { 
+      desc = 'NeoTree Buffer Reveal', silent = true 
+    })
   end
 }
