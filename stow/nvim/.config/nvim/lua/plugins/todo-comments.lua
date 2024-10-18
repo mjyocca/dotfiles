24dac1c843d34ac481f182680789f922596cf107
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2024-10-17 22:15:21 -0700

    feat(nvim): add todo-comments plugin

---
+++ stow/nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,5 @@
+return {
+  -- NOTE: this is an example note
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+}
