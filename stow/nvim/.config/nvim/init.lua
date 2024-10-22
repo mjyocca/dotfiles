21be11d767f356e5d3a702778f0f5140f5c9bda5
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2024-10-21 21:06:58 -0700

    feat(nvim): support vim-plugin manager

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -16,6 +16,24 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local data_dir = vim.fn.has('nvim') == 1 and vim.fn.stdpath('data') .. '/site' or '~/.vim'
+if vim.fn.empty(vim.fn.glob(data_dir .. '/autoload/plug.vim')) == 1 then
+  vim.fn.system({'curl', '-fLo', data_dir .. '/autoload/plug.vim', '--create-dirs', 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'})
+  vim.cmd [[
+    augroup PlugInstall
+      autocmd!
+      autocmd VimEnter * lua vim.cmd('PlugInstall --sync | source $MYVIMRC')
+    augroup END
+  ]]
+end
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug 'tpope/vim-commentary'
+
+vim.call('plug#end')
+
 require("vim-options")
 require("lazy").setup('plugins')
 
