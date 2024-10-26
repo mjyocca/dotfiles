ab280add3a39014372710d97308bc468d0dddc37
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2024-10-26 13:16:32 -0700

    chore(asdf): update tool versions

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -17,6 +17,7 @@ asdf_plugins() {
 	info "Installing asdf plugins..."
 	if asdf version >/dev/null; then
 		asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+    asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git
 		asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 		asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
 		asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
