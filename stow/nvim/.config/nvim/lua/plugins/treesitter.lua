17a58a87b3aa12a1e1b2f103de5ee4c6c9910085
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2024-11-03 11:17:57 -0800

    feat(nvim): add which-key plugin
    Adds which-key neovim plugin that shows pending keybinds. This will be
    helpful when as we're still starting out with neovim. Newb alert.

--- stow/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ stow/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      auto_install = true,
-      ensure_installed = { "lua", "html", "css", "javascript", "ruby", "python", "go", "rust", "c" },
-      sync_install = true,
-      highlight = { enabled = true },
-      indent = { enabled = true },
-    })
-  end
+  main = 'nvim-treesitter.configs',
+  opts = {
+    ensure_installed = { 'bash', 'c', 'diff', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'html', 'css', 'javascript', 'typescript', 'ruby', 'go' },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } }
+  },
 }
