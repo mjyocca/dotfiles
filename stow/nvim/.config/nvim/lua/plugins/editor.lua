6d4e721313b158d80f823cb20fee79431643e89f
Michael Yocca <michael.yocca@hashicorp.com>
2024-11-08 15:55:40 -0800

    feat(nvim): add modified files picker to telescope

--- stow/nvim/.config/nvim/lua/plugins/editor.lua
+++ stow/nvim/.config/nvim/lua/plugins/editor.lua
@@ -66,6 +66,35 @@ return {
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
+			-- Custom telescope picker to show current modified files
+			vim.keymap.set("n", "<leader>sm", function()
+				local previewers = require("telescope.previewers")
+				local pickers = require("telescope.pickers")
+				local sorters = require("telescope.sorters")
+				local finders = require("telescope.finders")
+				pickers
+					.new({}, {
+						results_title = "Modified in current branch",
+						finder = finders.new_oneshot_job({
+							"git",
+							"diff",
+							"--name-only",
+						}, {}),
+						sorter = sorters.get_fuzzy_file(),
+						previewer = previewers.new_termopen_previewer({
+							get_command = function(entry)
+								return {
+									"git",
+									"diff",
+									"--",
+									entry.value,
+								}
+							end,
+						}),
+					})
+					:find()
+			end, { desc = "[S]earch [M]odified files" })
+
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
 				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
@@ -85,9 +114,9 @@ return {
 			end, { desc = "[S]earch [/] in Open Files" })
 
 			-- Shortcut for searching your Neovim configuration files
-			vim.keymap.set("n", "<leader>sm", function()
+			vim.keymap.set("n", "<leader>sc", function()
 				builtin.find_files({ cwd = vim.fn.stdpath("config") })
-			end, { desc = "[S]earch [M]y [N]eovim files" })
+			end, { desc = "[S]earch Neovim [C]onfig files" })
 		end,
 	},
 
