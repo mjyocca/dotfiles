05e2530457b1973565cf542710553bbe175588e5
Michael Yocca <michael.yocca@hashicorp.com>
2024-12-06 11:28:20 -0800

    fix(nvim): golang autoformat, lsp, extras

--- stow/nvim/.config/nvim/lua/plugins/formatting.lua
+++ stow/nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,8 +1,16 @@
 return {
 	{
 		"nvimtools/none-ls.nvim",
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = { ensure_installed = { "gomodifytags", "goimports", "gofmt", "impl" } },
+			},
+		},
 		config = function()
 			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
@@ -14,8 +22,25 @@ return {
 					null_ls.builtins.formatting.clang_format,
 					null_ls.builtins.formatting.gofmt,
 					null_ls.builtins.formatting.goimports,
+					null_ls.builtins.code_actions.gomodifytags,
+					null_ls.builtins.code_actions.impl,
 					null_ls.builtins.formatting.yamlfmt,
 				},
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({
+							group = augroup,
+							buffer = bufnr,
+						})
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ bufnr = bufnr })
+							end,
+						})
+					end
+				end,
 			})
 
 			vim.keymap.set("n", "<leader>nf", vim.lsp.buf.format, { desc = "null_ls Format" })
