81c41158ebd66979f560b8feff802400510562f1
Michael Yocca <michael.yocca@hashicorp.com>
2025-01-15 22:29:52 -0800

    chore(nvim): cleanup snacks.nvim usage

--- stow/nvim/.config/nvim/lua/plugins/snacks.lua
+++ stow/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,31 +1,6 @@
-local ascii_art = [[
-=================     ===============     ===============   ========  ========
-\\ . . . . . . .\\   //. . . . . . .\\   //. . . . . . .\\  \\. . .\\// . . //
-||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\/ . . .||
-|| . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||
-||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||
-|| . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\ . . . . ||
-||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\_ . .|. .||
-|| . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\ `-_/| . ||
-||_-' ||  .|/    || ||    \|.  || `-_|| ||_-' ||  .|/    || ||   | \  / |-_.||
-||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \  / |  `||
-||    `'         || ||         `'    || ||    `'         || ||   | \  / |   ||
-||            .===' `===.         .==='.`===.         .===' /==. |  \/  |   ||
-||         .=='   \_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \/  |   ||
-||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \/  |   ||
-||   .=='    _-'          '-__\._-'         '-_./__-'         `' |. /|  |   ||
-||.=='    _-'                                                     `' |  /==.||
-=='    _-'                        N E O V I M                         \/   `==
-\   _-'                                                                `-_   /
- `''                                                                      ``'
-]]
-
 local dashboard = {
 	enabled = true,
 	pane_gap = 8,
-	-- preset = {
-	-- 	header = ascii_art,
-	-- },
 	sections = {
 		{ section = "header" },
 		{ section = "keys", gap = 1, padding = 1 },
@@ -47,20 +22,20 @@ local dashboard = {
 			indent = 2,
 			padding = 1,
 		},
-		{
-			pane = 2,
-			icon = " ",
-			title = "Git Status",
-			section = "terminal",
-			enabled = function()
-				return require("snacks").git.get_root() ~= nil
-			end,
-			cmd = "git status --short --branch --renames",
-			height = 5,
-			padding = 1,
-			ttl = 5 * 60,
-			indent = 2,
-		},
+		-- {
+		-- 	pane = 2,
+		-- 	icon = " ",
+		-- 	title = "Git Status",
+		-- 	section = "terminal",
+		-- 	enabled = function()
+		-- 		return require("snacks").git.get_root() ~= nil
+		-- 	end,
+		-- 	cmd = "git status --short --branch --renames",
+		-- 	height = 5,
+		-- 	padding = 1,
+		-- 	ttl = 5 * 60,
+		-- 	indent = 2,
+		-- },
 		{ section = "startup" },
 	},
 }
@@ -83,6 +58,9 @@ return {
 		toggle = { enabled = true },
 		scratch = { enabled = true },
 		debug = { enabled = true },
+		terminal = { enabled = true },
+		git = { enabled = true },
+		words = {},
 		gitbrowse = { enabled = true },
 		lazygit = {
 			enabled = true,
