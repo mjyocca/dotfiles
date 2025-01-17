9507f20f63226a388f9d2b3b97eeb4f6732caf20
Michael Yocca <michael.yocca@hashicorp.com>
2025-01-16 18:03:42 -0800

    fix(nvim/lsp): override default timeout for lsp formatting on save

--- stow/nvim/.config/nvim/lua/plugins/formatting.lua
+++ stow/nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,58 +1,61 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = { ensure_installed = { "gomodifytags", "goimports", "gofmt", "impl" } },
-      },
-    },
-    config = function()
-      local null_ls = require("null-ls")
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = { ensure_installed = { "gomodifytags", "goimports", "gofmt", "impl" } },
+			},
+		},
+		config = function()
+			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.shellharden,
-          null_ls.builtins.diagnostics.erb_lint,
-          null_ls.builtins.diagnostics.rubocop,
-          null_ls.builtins.formatting.rubocop,
-          null_ls.builtins.formatting.clang_format,
-          null_ls.builtins.formatting.gofmt,
-          null_ls.builtins.formatting.goimports,
-          null_ls.builtins.code_actions.gomodifytags,
-          null_ls.builtins.code_actions.impl,
-          null_ls.builtins.formatting.yamlfmt,
-        },
-        on_attach = function(client, bufnr)
-          -- Specify filetypes where formatting should be disabled
-          local disabled_filetypes = { "ruby", "markdown" }
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.shellharden,
+					null_ls.builtins.diagnostics.erb_lint,
+					null_ls.builtins.diagnostics.rubocop,
+					null_ls.builtins.formatting.rubocop,
+					null_ls.builtins.formatting.clang_format,
+					null_ls.builtins.formatting.gofmt,
+					null_ls.builtins.formatting.goimports,
+					null_ls.builtins.code_actions.gomodifytags,
+					null_ls.builtins.code_actions.impl,
+					null_ls.builtins.formatting.yamlfmt,
+				},
+				on_attach = function(client, bufnr)
+					-- Specify filetypes where formatting should be disabled
+					local disabled_filetypes = { "ruby", "markdown" }
 
-          -- Check if current filetype is in the disabled list
-          local filetype = vim.bo.filetype -- vim.api.nvim_buf_get_option(bufnr, "filetype")
-          if vim.tbl_contains(disabled_filetypes, filetype) then
-            return
-          end
+					-- Check if current filetype is in the disabled list
+					local filetype = vim.bo.filetype -- vim.api.nvim_buf_get_option(bufnr, "filetype")
+					if vim.tbl_contains(disabled_filetypes, filetype) then
+						return
+					end
 
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({
-              group = augroup,
-              buffer = bufnr,
-            })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
-              end,
-            })
-          end
-        end,
-      })
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({
+							group = augroup,
+							buffer = bufnr,
+						})
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({
+									bufnr = bufnr,
+									timeout_ms = 3000, -- LSP formatting was timing out, default is `1000`.
+								})
+							end,
+						})
+					end
+				end,
+			})
 
-      vim.keymap.set("n", "<leader>nf", vim.lsp.buf.format, { desc = "null_ls Format" })
-    end,
-  },
+			vim.keymap.set("n", "<leader>nf", vim.lsp.buf.format, { desc = "null_ls Format" })
+		end,
+	},
 }
