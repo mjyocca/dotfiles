7a2bfc294b10b56f6d481e91dabe66e1f667a99a
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2025-04-04 17:55:51 -0700

    chore(scripts): add java asdf plugin

--- scripts/plugins.sh
+++ scripts/plugins.sh
@@ -10,16 +10,17 @@ tmux_plugin_manager() {
 		warn "tmux plugin manager already installed"
 	else
 		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	fi 
+	fi
 }
 
 asdf_plugins() {
 	info "Installing asdf plugins..."
 	if asdf version >/dev/null; then
 		asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-    asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git
+		asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git
 		asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 		asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+		asdf plugin add java https://github.com/halcyon/asdf-java.git
 		asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
 	else
 		error "asdf is not installed"
