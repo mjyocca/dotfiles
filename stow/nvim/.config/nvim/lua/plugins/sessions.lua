ebdc8f6963d0e9a6961881ace1b6b2cd3efa50d6
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2025-04-05 15:48:25 -0700

    feat(nvim): enable telescope lens for auto-sessions

--- stow/nvim/.config/nvim/lua/plugins/sessions.lua
+++ stow/nvim/.config/nvim/lua/plugins/sessions.lua
@@ -1,12 +1,19 @@
 return {
   "rmagatti/auto-session",
   lazy = false,
-
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
+  keys = {
+    -- Will use Telescope if installed or a vim.ui.select picker otherwise
+    { "<leader>wr", "<cmd>SessionSearch<CR>",         desc = "Session search" },
+    { "<leader>ws", "<cmd>SessionSave<CR>",           desc = "Save session" },
+    { "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+  },
   opts = {
     suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-    -- log_level = 'debug',
+    session_lens = {
+      buftypes_to_ignore = {},
+      load_on_setup = true,
+      theme_conf = { border = true },
+      previewer = false,
+    },
   },
 }
