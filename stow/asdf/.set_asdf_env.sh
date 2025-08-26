14d99e35c44e11fbe6ece559e8828941bee7df0a
Michael Yocca <michael.yocca@hashicorp.com>
2025-08-25 19:00:51 -0700

    feat(asdf): add script to export asdf language versions for globally spawned shells

---
+++ stow/asdf/.set_asdf_env.sh
@@ -0,0 +1,13 @@
+# NOTE: Some subprocesses spawned within subschells don't always inherit configuration from current working directory.
+# For example mason.nvim spawns subshell by Neovim where the working directory is not inherited and defaults to the root directory.
+# In these cases, if there is no global language version, installation may fail for the LSP or packages.
+#
+# This script is meant to be executed from direnv .envrc file as `source ~/.set_asdf_env.sh`
+# Exports all .tool-version entries to the current shells environemnt prior to starting Neovim. 
+# This should allow spawned subshell within neovim to inherit these values as well.
+if [ -f .tool-versions ]; then
+  while read lang ver; do
+    export ASDF_$(echo $lang | tr '[:lower:]' '[:upper:]')_VERSION=$ver
+  done < .tool-versions
+fi
+
