b453a0eb7fc110ead474c1530e70c358f4f665b1
Michael Yocca <michael.yocca@hashicorp.com>
2025-10-04 20:33:40 -0700

    feat(nvim): update snacks config

--- stow/nvim/.config/nvim/lua/plugins/snacks.lua
+++ stow/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -45,23 +45,42 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
+    toggles = {
+      follow = "f",
+      hidden = "h",
+      ignored = "i",
+      modified = "m",
+      regex = { icon = "R", value = false },
+    },
+    matcher = { frecency = true },
     picker = {
+      -- debug = { scores = true },
+      matcher = { frecency = true },
       icons = {
         files = {
-          dir = "",
-          dir_open = "",
+          -- dir = "",
+          -- dir_open = "",
+          dir = " ",
+          dir_open = " ",
         },
         ui = {
           live = "󰐰 ",
-          hidden = "h",
-          ignored = "i",
+          hidden = " ",
+          ignored = " ",
           follow = "f",
           selected = "● ",
           unselected = "○ ",
-          -- selected = " ",
         },
       },
       sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+        grep = {
+          hidden = true,
+          ignored = true,
+        },
         explorer = {
           hidden = true,
           ignored = true,
@@ -80,8 +99,8 @@ return {
                 win = "input",
                 height = 1,
                 border = "rounded",
-                title = "{title} {live} {flags}",
-                title_pos = "center",
+                title = "{flags}",
+                title_pos = "right",
               },
               { win = "list",    border = "none" },
               { win = "preview", title = "{preview}", height = 0.4, border = "top" },
@@ -101,14 +120,29 @@ return {
     notifier = { enabled = true },
     dashboard = dashboard,
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
+    statuscolumn = {
+      left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+      right = { "fold", "git" }, -- priority of signs on the right (high to low)
+      folds = {
+        open = false, -- show open fold icons
+        git_hl = false, -- use Git Signs hl for fold icons
+      },
+      git = {
+        -- patterns to match Git signs
+        patterns = { "GitSign", "MiniDiffSign" },
+      },
+      refresh = 50, -- refresh at most every 50ms
+    },
     indent = {
       enabled = true,
       only_scope = true,
       only_current = true,
     },
     scope = { enabled = true },
-    toggle = { enabled = true },
+    toggle = {
+      enabled = true,
+      which_key = true,
+    },
     scratch = { enabled = true },
     debug = { enabled = true },
     terminal = { enabled = true },
@@ -143,11 +177,32 @@ return {
       desc = "[S]earch Open [B]uffers",
     },
     {
+      "<leader><leader>",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "[S]earch Open [B]uffers",
+    },
+    {
       "<leader>sF",
       function()
         Snacks.picker.smart()
       end,
-      desc = "[S]mart Find",
+      desc = "[S]mart [F]ind Files",
+    },
+    {
+      "<leader>sf",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = "[S]earch Files",
+    },
+    {
+      "<leader>sg",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = "[S]earch by [G]rep"
     },
     {
       "<leader>sC",
@@ -156,6 +211,20 @@ return {
       end,
       desc = "[S]earch [C]olorschemes",
     },
+    {
+      "<leader>sI",
+      function()
+        Snacks.picker.icons()
+      end,
+      desc = "[S]earch [I]cons",
+    },
+    {
+      "<leader>sH",
+      function()
+        Snacks.picker.highlights()
+      end,
+      desc = "[S]earch [H]ighlights",
+    }
   },
   init = function()
     require("config.autocmds").snacks({})
