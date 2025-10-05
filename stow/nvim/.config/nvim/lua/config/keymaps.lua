35271dc6568248f9e05325a576ddf92a664ff255
Michael Yocca <michael.yocca@hashicorp.com>
2025-10-04 20:33:18 -0700

    feat(nvim): replace telescope keymaps with snack pickers

--- stow/nvim/.config/nvim/lua/config/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,15 +1,15 @@
 local M = {}
 
--- General utility function to simplify keymap creation
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
 function M.general()
+  -- General utility function to simplify keymap creation
+  local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+      options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  end
+
   -- vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
   map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
@@ -23,43 +23,54 @@ function M.general()
   map("v", ">", ">gv", { noremap = true, silent = true })
   map("v", "<", "<gv", { noremap = true, silent = true })
 
-  vim.api.nvim_set_keymap("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
-  vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true })
+  -- Navigate between open buffers
+  map("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
+  map("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true })
 end
 
 -- LSP KeyMaps
 function M.lsp(event)
-  local map = function(keys, func, desc, mode)
+  local function map(keys, func, desc, mode)
     mode = mode or "n"
     vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
   end
   -- Jump to the definition of the word under your cursor.
   --  This is where a variable was first declared, or where a function is defined, etc.
   --  To jump back, press <C-t>.
-  map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+  -- map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+  map("gd", require("snacks").picker.lsp_definitions, "[G]oto [D]efinition")
+
+  -- Jump to declarations
+  map("gD", require("snacks").picker.lsp_declarations, "[G]oto [D]eclaration")
+
 
   -- Find references for the word under your cursor.
-  map("gR", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  map("gR", require("snacks").picker.lsp_references, "[G]oto [R]eferences")
+  -- leaving for now to compare
   map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
   -- Jump to the implementation of the word under your cursor.
   --  Useful when your language has ways of declaring types without an actual implementation.
-  map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+  -- map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+  map("gI", require("snacks").picker.lsp_implementations, "[G]oto [I]mplementation")
 
   -- NOTE: map("K", vim.lsp.buf.hover, "Default Keymap for LSP hover reveal")
 
   -- Jump to the type of the word under your cursor.
   --  Useful when you're not sure what type a variable is and you want to see
   --  the definition of its *type*, not where it was *defined*.
-  map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+  -- map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+  map("gy", require("snacks.picker").lsp_type_definitions, "[G]o T[y]pe Definition")
 
   -- Fuzzy find all the symbols in your current document.
   --  Symbols are things like variables, functions, types, etc.
-  map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+  -- map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+  map("<leader>ds", require("snacks.picker").lsp_symbols, "LSP [D]ocument [S]ymbols")
 
   -- Fuzzy find all the symbols in your current workspace.
   --  Similar to document symbols, except searches over your entire project.
-  map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+  -- map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+  map("<leader>ws", require("snacks.picker").lsp_workspace_symbols, "LSP [W]orkspace [S]ymbols")
 
   -- Rename the variable under your cursor.
   --  Most Language Servers support renaming across files, etc.
@@ -71,7 +82,7 @@ function M.lsp(event)
 
   -- WARN: This is not Goto Definition, this is Goto Declaration.
   --  For example, in C this would take you to the header.
-  map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+  -- map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration vim.lsp.buf.declaration")
 
   local client = vim.lsp.get_client_by_id(event.data.client_id)
   -- The following code creates a keymap to toggle inlay hints in your
