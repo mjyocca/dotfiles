014738ad31f4615e22af6f4f83da264d1463c342
Michael Yocca <michael.yocca@hashicorp.com>
2026-03-30 11:12:30 -0700

    feat(nvim): add markdown preview for md/mdx filetypes

--- stow/nvim/.config/nvim/lua/plugins/editor.lua
+++ stow/nvim/.config/nvim/lua/plugins/editor.lua
@@ -455,6 +455,19 @@ return {
     end,
   },
 
+  {
+      'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
+      opts = {},
+      config = function()
+        require("render-markdown").setup {
+          file_types = { "markdown", "mdx" },
+        }
+      end,
+  },
+
   -- Game to get good at vim
   {
     "ThePrimeagen/vim-be-good",
