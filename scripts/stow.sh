00fd3fffcec09a3c63b5788b06a9d0dac88830a1
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2026-04-03 14:06:35 -0700

    refactor(packages): migrate stow/* to packages/*

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -3,9 +3,9 @@
 . $PWD/scripts/utils.sh
 
 stow_dotfiles() {
-	local to_stow="$(find stow -maxdepth 1 -type d -mindepth 1 | awk -F "/" '{print $NF}' ORS=' ')"
+	local to_stow="$(find packages -maxdepth 1 -type d -mindepth 1 | awk -F "/" '{print $NF}' ORS=' ')"
 	info "Stowing: $to_stow"
-	stow --dir stow --verbose 2 --target "$HOME" $to_stow
+	stow --dir packages --verbose 2 --target "$HOME" $to_stow
 }
 
 # Check if the script is being run directly or sourced
