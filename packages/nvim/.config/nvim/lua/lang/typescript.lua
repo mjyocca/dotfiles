c628f65762fb756dc26e7c878828b2cf139c465f
Michael Yocca <19916665+mjyocca@users.noreply.github.com>
2026-04-03 15:52:34 -0700

    chore(nvim): format typescript lang lsp file

--- packages/nvim/.config/nvim/lua/lang/typescript.lua
+++ packages/nvim/.config/nvim/lua/lang/typescript.lua
@@ -2,20 +2,15 @@ local typescript = {
 	server_name = "ts_ls",
 	lspconfig = {
 		filetypes = {
-      'javascript',
-      'javascriptreact',
-      'javascript.jsx',
-      'typescript',
-      'typescriptreact',
-      'typescript.tsx',
+			"javascript",
+			"javascriptreact",
+			"javascript.jsx",
+			"typescript",
+			"typescriptreact",
+			"typescript.tsx",
 		},
 	},
-  -- root_dir = require('lspconfig.util').root_pattern(
-  --   'tsconfig.json',
-  --   'jsconfig.json',
-  --   'package.json',
-  --   '.git'
-  -- ),
+	root_dir = require("lspconfig.util").root_pattern("tsconfig.json", "jsconfig.json", "package.json", ".git"),
 }
 
 return typescript
